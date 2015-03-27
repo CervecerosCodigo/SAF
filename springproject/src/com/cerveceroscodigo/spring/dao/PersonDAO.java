e3a0e7f9d40c8be6451e80b3a7b5590d0d99c72c
Espen Zaal <espenbz@gmail.com>
2015-03-27 15:59:38 +0100

    Lagt til gitignore

--- springproject/src/com/cerveceroscodigo/spring/dao/PersonDAO.java
+++ springproject/src/com/cerveceroscodigo/spring/dao/PersonDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,11 @@ public class PersonDAO {
 
 	
 	private NamedParameterJdbcTemplate jdbc;
+
+	
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 	
 	@Autowired
 	public void setDatasource(DataSource jdbc) {
@@ -20,8 +27,15 @@ public class PersonDAO {
 	}
 	
 	
+	public Session session(){
+		return sessionFactory.getCurrentSession();
+	}
+
+	
+	//from Person er referanse til Java-klasse, ikke database
+	@SuppressWarnings("unchecked")
 	public List<Person> getPersons(){
-		return null;
+		return session().createQuery("from Person").list();
 	}
 	
 	public Person getPerson(int id){
